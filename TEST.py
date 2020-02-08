da69b0620d81a6d20c40b7996aed6f29107cb3a9
Nouamane Tazi <nouamane98@gmail.com>
2020-02-08 12:31:25 +0100

    Implement is_checkmate

--- TEST.py
+++ TEST.py
@@ -13,12 +13,13 @@ class AIPlayer(Player):
         self.p1_id = -1
         self.p2_id = 1
         self.ref_table = np.array([[3,4,5,7,5,4,3],[4,6,8,10,8,6,4],[5,8,11,13,11,8,5],[5,8,11,13,11,8,5],[4,6,8,10,8,6,4],[3,4,5,7,5,4,3]])
-        self.depth = 3
+        self.depth = 4
+        self.depth = 4
+        self.depth = 4
 
     def getColumn(self, board):
         _, best_move = self.maximize(board, -math.inf, math.inf, 0)
-        error("BEST MOVE")
-        error(best_move)
+        # error("BEST MOVE = "+str(best_move))
         return best_move
 
 
@@ -46,7 +47,7 @@ class AIPlayer(Player):
 
         for col in board.getPossibleColumns(): # p2 moves
             if alpha >= beta:
-                return alpha
+                return beta
             board_copy = deepcopy(board)
             board_copy.play(self.p2_id, col)
             new_beta, _ = self.maximize(board_copy, alpha, beta, level)
@@ -56,6 +57,13 @@ class AIPlayer(Player):
 
 
     def get_score(self, board):
+        checkmate = self.is_checkmate(board)
+        if checkmate:
+            # error("*"*20)
+            # error(board)
+            # error(checkmate)
+            return checkmate
+
         list_board = board.board
         score = 0
         for i in range(board.num_rows):
@@ -64,14 +72,39 @@ class AIPlayer(Player):
                     score += self.ref_table[i,j]
                 elif list_board[j][i] == self.p2_id:
                     score -= self.ref_table[i,j]
-        error(board)
-        error(score)
+        # error(board)
+        # error(score)
         return score
 
+    def test_win(self, list):
+        for i in range(len(list)-4):
+            if [1,1,1,1] == list[i:i+4]:
+                return -math.inf
+            elif [-1,-1,-1,-1] == list[i:i+4]:
+                return math.inf
+
     def is_checkmate(self, board):
-        pass
+        for c in range(board.num_cols):
+            col = board.getCol(c)
+            val = self.test_win(col)
+            if val: return val
 
+        up = True
+        for shift in range(-2,4): # TODO generalize for num_cols
+            diag = board.getDiagonal(up,shift)
+            val = self.test_win(diag)
+            if val: return val
 
+        up = False
+        for shift in range(3,10): # Downwards diags
+            diag = board.getDiagonal(up,shift)
+            val = self.test_win(diag)
+            if val: return val
+
+        for r in range(board.num_rows):
+            row = board.getRow(r)
+            val = self.test_win(row)
+            if val: return val
 
     """def alpha_beta(self, board, player, alpha, beta, level): #explore branches
         if level >= self.depth:
